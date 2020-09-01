c44f90e0be33c43711ff94ae32c4382297935737
Tichalo <ticha.muma@chatdesk.com>
2020-09-01 15:34:45 +0100

    .

--- school/school/settings.py
+++ school/school/settings.py
@@ -51,8 +51,6 @@ MIDDLEWARE = (
     'whitenoise.middleware.WhiteNoiseMiddleware'
 )
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 ROOT_URLCONF = 'school.urls'
 
 
